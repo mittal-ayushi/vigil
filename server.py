a433faa61d1bd42baccb171c94f631b8f9292989
mittal-ayushi <ayushi.mittal.318@gmail.com>
2026-08-24 17:35:24 +0530

    Added CSS

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-binarypath = os.environ.get("vigilbinary", "./main")
+binarypath = os.path.abspath(os.environ.get("vigilbinary", "./main"))
 
 @app.route("/scan", methods=["POST"])
 def scan():
